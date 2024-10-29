44cebc4d67643a39c5417c840ba7a2126cf39ebc
Oleksandr V <dev.ovoronkov@gmail.com>
2024-10-29 21:00:35 +0100

    add ping-pong endpoint

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, "pong")
+	})
+
+	if err := r.Run(":8080"); err != nil {
+		panic(err)
+	}
+}
