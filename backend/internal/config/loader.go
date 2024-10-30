0abd18f269a083c5bbd5d85424aaf6344e8b8bc6
Oleksandr V <dev.ovoronkov@gmail.com>
2024-10-30 02:19:30 +0100

    fix: dont read file in production

--- backend/internal/config/loader.go
+++ backend/internal/config/loader.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 var AppConfig *Config
@@ -10,13 +11,21 @@ var AppConfig *Config
 func LoadConfig() (*Config, error) {
 	var config Config
 
-	viper.SetConfigFile("env.development.yaml")
-	viper.SetConfigType("yaml")
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file, %s", err)
+	env := os.Getenv("RAILWAY_ENVIRONMENT")
+	if env == "" {
+		env = "development"
 	}
+
+	if env == "development" {
+		viper.SetConfigFile("env.development.yaml")
+		viper.SetConfigType("yaml")
+
+		if err := viper.ReadInConfig(); err != nil {
+			log.Printf("Warning: Config file not found: %v", err)
+		}
+	}
+
+	viper.AutomaticEnv()
 
 	if err := viper.Unmarshal(&config); err != nil {
 		return nil, err
